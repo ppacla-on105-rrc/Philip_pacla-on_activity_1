9d9937e4bf6edfe51dce2e13a13e212f98beac39
Philip Pacla-on <ppacla-on105@academic.rrc.ca>
2025-01-17 15:07:47 -0600

    Updated client program to match the newly added things in the LibraryItem class.

--- activity_01_main.py
+++ activity_01_main.py
@@ -21,25 +21,29 @@ def main():
     # 1. Code a statement which creates an instance of the LibraryItem class with valid inputs.
     # Use your own unique valid values for the inputs to the class.
 
-    valid_item = LibraryItem("Harry Potter", "J.K Rowling", Genre.FANTASY)
+    valid_item = LibraryItem(
+        1, "Harry Potter", "J.K Rowling", Genre.FANTASY, False)
 
     # 2. Using the instance defined above, and the class Accessors, print
     # each of the attributes of the LibraryItem instance.
     try:
+        print(f"Item ID: {valid_item.item_id}")
         print(f"Title: {valid_item.title}")
         print(f"Author: {valid_item.author}")
         print(f"Genre: {valid_item.genre}")
+        print(f"Is Borrowed: {valid_item.is_borrowed}")
     except ValueError as e:
         print(f"Error: {e}")
     # 3. Code a statement which creates an instance of the LibraryItem class with one or more invalid inputs.
     # Use your own unique valid values for the inputs to the class.
     try:
-        invalid_item = LibraryItem("", "J.K Rowling", Genre.FANTASY)
+        invalid_item = LibraryItem(1, "", "J.K Rowling", Genre.FANTASY, False)
     except ValueError as e:
         print(f"Error: {e}")
 
     try:
-        invalid_item = LibraryItem("Harry Potter", "J.K Rowling", "INVALID")
+        invalid_item = LibraryItem(
+            1, "Harry Potter", "J.K Rowling", "INVALID", False)
     except ValueError as e:
         print(f"Error: {e}")
 
